0d3875976366ac88224ad25920c66960c2cacc78
EdwardHill15 <total-health@kpnmail.nl>
2022-06-29 10:23:07 +0200

    changes Rscript covid19 data

--- Rscript get owid covid19 data.R
+++ Rscript get owid covid19 data.R
@@ -1,7 +1,7 @@
-=====================================
+############################################################
 # how to get data from github Our World in data (OWID)
 # https://github.com/owid/covid-19-data
-=====================================
+############################################################
   
 url <- "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
 df <- read.csv(url)
